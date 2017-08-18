79289fa300092d8a5c73bb4ebdb15f78b6122b43
Sandrow Townsend <stownsend@sapient.com>
2017-08-18 09:26:04 -0400

    first git push

---
+++ script.js
@@ -0,0 +1,6 @@
+
+TweenMax.to(".prospectBody", 1.9, { y:-60, repeat:-1, yoyo:true, ease:Power1.easeInOut });
+
+TweenMax.to(".shadow", 1.9, { scale:0.8, repeat:-1, opacity:0.3, transformOrigin:"50% 50%", yoyo:true, ease:Power1.easeInOut });
+
+
